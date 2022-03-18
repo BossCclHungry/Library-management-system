93c99259b0db2c55aa9be63eb73d6bf3e645b80f
MrCao <1374954185@qq.com>
2022-03-18 16:41:29 +0800

    新建图书管理系统项目

---
+++ src/main/java/com/ccl/labrarymanager/LabraryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ccl.labrarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabraryManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabraryManagerApplication.class, args);
+    }
+
+}
